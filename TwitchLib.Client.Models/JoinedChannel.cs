1335157ecad2fb6909f8a12d52f90ae72e970df4
tetyys <tetyys@tetyys.com>
2019-07-27 21:02:40 +0300

    Remove stuff I don't like

--- TwitchLib.Client.Models/JoinedChannel.cs
+++ TwitchLib.Client.Models/JoinedChannel.cs
@@ -7,19 +7,11 @@
         public string Channel { get; }
         /// <summary>Object representing current state of channel (r9k, slow, etc).</summary>
         public ChannelState ChannelState { get; protected set; }
-        /// <summary>The most recent message received.</summary>
-        public ChatMessage PreviousMessage { get; protected set; }
 
         /// <summary>JoinedChannel object constructor.</summary>
         public JoinedChannel(string channel)
         {
             Channel = channel;
-        }
-
-        /// <summary>Handles a message</summary>
-        public void HandleMessage(ChatMessage message)
-        {
-            PreviousMessage = message;
         }
     }
 }
